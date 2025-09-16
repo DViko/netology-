64a99bc7d58ffb744352081569613897cbff9028
DViko <mymental.darkness@gmail.com>
2025-09-16 10:28:55 +0200

    upgrade

--- 1.4/01/main.cpp
+++ 1.4/01/main.cpp
@@ -3,6 +3,21 @@
 
 int main() {
     
+    int input_number{0}, result{0};
+    bool is_true{false};
+
+    do {
+        std::cout << "Inter a positive integer or 0 for exit and get result: ";
+        std::cin >> input_number;
+
+        if (input_number != 0)
+            result += input_number; 
+        else
+            is_true = true;
+
+    } while (!is_true);
+
+    std::cout << "The result is: " << result << std::endl;
 
     return EXIT_SUCCESS;
 }
