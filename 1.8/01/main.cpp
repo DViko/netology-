8fd3fced70192e32dd24b03b2b3d4b527c0b0820
DViko <mymental.darkness@gmail.com>
2025-10-03 12:46:45 +0200

    upgrade

---
+++ 1.8/01/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+void countingFunction();
+
+
+int main() {
+
+    for (int i = 0; i < 10; i ++)
+    {
+        countingFunction();
+    }
+
+    return EXIT_SUCCESS;
+}
+
+void countingFunction()
+{
+    static short count {0};
+
+    std::cout << "Number of calls to the countingFunction() : " << (++ count) << std::endl;
+}
---
+++ 1.8/02/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+namespace Math
+{
+    namespace addition
+    {
+        int doIt(int a, int b) { return a + b; }
+    }
+    namespace subtraction
+    {
+        int doIt(int a, int b) { return a - b; }
+    }
+    namespace multiplication
+    {
+        int doIt(int a, int b) { return a * b; }
+    }
+    namespace division
+    {
+        int doIt(int a, int b) { return a / b; }
+    }
+}
+
+void printLine(const std::string& operation, int value);
+
+
+int main()
+{
+
+    int x {15}, y {5};
+
+    std::cout << "x = " << x << ", y = " << y << "\n";
+
+    printLine("Addition", Math::addition::doIt(x, y));
+    printLine("Subtraction", Math::subtraction::doIt(x, y));
+    printLine("Multiplication", Math::multiplication::doIt(x, y));
+    printLine("Division", Math::division::doIt(x, y));
+
+    return EXIT_SUCCESS;
+}
+
+void printLine(const std::string& operation, int value)
+{
+    std::cout << operation << ": " << value << std::endl;
+}
